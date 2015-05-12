e1fc51124784ff4131f0aa6deeea97e4b1cfa9cc
Ivan Kruglov <ivan.kruglov@yahoo.com>
2015-05-12 12:43:53 +0200

    speed up timer startup procedure

--- lib/YAHC.pm
+++ lib/YAHC.pm
@@ -624,8 +624,9 @@ sub _set_until_state_timer {
     };
 
     _register_in_timeline($conn, "setting $timeout_name to %.3fs", $timeout) if $conn->{keep_timeline};
-    $watchers->{$timer_name} = $self->{loop}->timer($timeout, 0, $timer_cb);
-    $watchers->{$timer_name}->priority(-2); # set lowest priority
+    my $w = $watchers->{$timer_name} = $self->{loop}->timer_ns($timeout, 0, $timer_cb);
+    $w->priority(-2); # set lowest priority
+    $w->start;
 }
 
 ################################################################################
