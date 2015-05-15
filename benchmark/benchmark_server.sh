8474fb3f9bf794b361da1537a27fdf62809b84af
Ivan Kruglov <ivan.kruglov@yahoo.com>
2015-05-15 23:18:54 +0200

    reorganize benchmark code

--- benchmark/benchmark_server.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-starman --host 127.0.0.1 --workers 16 bench.psgi
---
+++ benchmark/server.pl
@@ -0,0 +1,16 @@
+#!/usr/bin/env perl
+
+use strict;
+use warnings;
+
+use Getopt::Long qw(GetOptions);
+
+my $host = '127.0.0.1';
+my $workers = 16;
+
+GetOptions(
+    'host=s'        => \$host,
+    'workers=i'     => \$workers,
+) or die "bad option";
+
+exec("starman --host $host --workers $workers bench.psgi");
